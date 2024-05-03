07403bc74e143e5cfc088609134ac34a7c217d20
Akari <barryofc11@gmail.com>
2024-05-03 22:39:09 +0000

    add draft_release.py
    Signed-off-by: Akari <barryofc11@gmail.com>

---
+++ draft_release.py
@@ -0,0 +1,53 @@
+import json
+import os
+import requests
+from update import (
+    get_tag_body,
+    get_tag_release
+)
+
+owner = 'moekernel'
+repo = 'clang'
+tag_name = get_tag_release()
+target_commitish = 'main'
+name = get_tag_release()
+body = get_tag_body()
+draft = False
+prerelease = False
+
+token = os.getenv('TOKEN_GITHUB', '')
+
+def draft_release():
+
+    url = f'https://api.github.com/repos/{owner}/{repo}/releases'
+
+    headers = {
+        'Authorization': f'token {token}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
+
+    data = {
+        'tag_name': tag_name,
+        'target_commitish': target_commitish,
+        'name': name,
+        'body': body,
+        'draft': draft,
+        'prerelease': prerelease
+    }
+
+    response = requests.post(
+        url,
+        headers=headers,
+        data=json.dumps(data)
+    )
+
+    if response.status_code == 201:
+        print('Novo lançamento criado com sucesso!')
+    else:
+        print(
+            'Erro ao criar o lançamento:', 
+            response.status_code, 
+            response.text
+        )
+
+draft_release()
