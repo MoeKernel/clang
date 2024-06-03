62aace28c27d2376b918aa85b1dd1c82c80ce3b5
Akari <barryofc11@gmail.com>
2024-06-03 19:52:20 +0000

    clang.sh: added verification if exist clang folder.
    Signed-off-by: Akari <barryofc11@gmail.com>

--- clang.sh
+++ clang.sh
@@ -14,6 +14,11 @@ url="https://github.com/$user/Clang/releases/download/$version/$clang_gz"
 output_file="$clang_gz"
 destination_dir="$HOME/tc/clang-19.0.0"
 
+if [ -d "$destination_dir" ]; then
+  echo "The directory '$destination_dir' already exists. Exiting script."
+  exit 0
+fi
+
 if ! command -v curl &>/dev/null; then
   echo "The command 'curl' not installed. Please, install for continue."
   sudo apt update -y 
