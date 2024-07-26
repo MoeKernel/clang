9863935db3493d782bef4ec9f9ce7a45383b1d89
Akari <akariondev@gmail.com>
2024-07-26 18:42:12 -0300

    clang.sh: update variables to set clang: clang-20.0.0

--- clang.sh
+++ clang.sh
@@ -9,11 +9,11 @@ user="ZyCromerZ" # GitHub user.
 date_=$(cat date_.txt) # updated clang version.
 version_clang="20.0.0git"
 version="$version_clang-$date_-release"
-clang_gz="Clang-19.0.0git-$date_.tar.gz"
+clang_gz="Clang-$version_clang-$date_.tar.gz"
 url="https://github.com/$user/Clang/releases/download/$version/$clang_gz"
 
 output_file="$clang_gz"
-destination_dir="$HOME/tc/clang-19.0.0"
+destination_dir="$HOME/tc/clang-20.0.0"
 
 if [ -d "$destination_dir" ]; then
   echo "The directory '$destination_dir' already exists. Exiting script."
